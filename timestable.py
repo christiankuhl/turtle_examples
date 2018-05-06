bcab69adf3fc0a552b7f61eeccd31b308c087737
Christian Kuhl <christian.kuhl84@gmail.com>
2018-05-06 14:04:45 +0200

    Times table

--- timestable.py
+++ timestable.py
@@ -35,7 +35,7 @@ def coords_from_int(n):
             -radius * cos(n/modulus*2*pi)]
 
 # Get input
-operand = int(numinput("Einmaleins.", "Reihe:", 3, minval=2, maxval=10))
+operand = int(numinput("Einmaleins.", "Reihe:", 2, minval=2, maxval=20))
 
 # Do the actual math
 turtle.color("blue")
@@ -49,5 +49,4 @@ for a in range(modulus):
         result = (result * operand) % modulus
         turtle.pendown()
         turtle.setpos(*coords_from_int(result))
-
 done()
